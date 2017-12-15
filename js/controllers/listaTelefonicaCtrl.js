7e77ba697f8e8db7d3de0a4fabe9d6aad9b416c5
Thiago Luiz Francisquete <thiago.francisquete@gmail.com>
2017-12-15 17:43:05 -0200

    add app.js and API and controllers folder

---
+++ js/controllers/listaTelefonicaCtrl.js
@@ -0,0 +1,44 @@
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, $http) {
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [];
+
+    var carregarContatos = function () {
+        $http.get('http://localhost:3412/contatos').then(function (response) {
+            $scope.contatos = response.data;
+        });
+    };
+
+    var carregarOperadoras = function () {
+        $http.get('http://localhost:3412/operadoras').then(function (response) {
+            $scope.operadoras = response.data;
+        }).catch(function (response) {
+            $scope.message = 'Erro: ' + response.data;
+        });
+    };
+
+    $scope.adicionarContato = function (contato) {
+        contato.data = new Date();
+        $http.post('http://localhost:3412/contatos', contato).then(function (response) {
+            delete $scope.contato;
+            $scope.contatoForm.$setPristine();
+            carregarContatos();
+        });
+    };
+    $scope.apagarContatos = function (contatos) {
+        $scope.contatos = contatos.filter(function (contato) {
+            if (!contato.selecionado) return contato;
+        });
+    };
+    $scope.isContatoSelecionado = function (contatos) {
+        return contatos.some(function (contato) {
+            return contato.selecionado;
+        });
+    };
+    $scope.ordernarPor = function (campo) {
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+
+    carregarContatos();
+    carregarOperadoras();
+});
